0f85066ead995640bca0ba6307c58dffbf0bfbe1
KimDoyeon <kdy0962@naver.com>
2018-12-07 17:24:19 +0900

    kdy0962 : 1. 토너먼트 프래그먼트 생성

---
+++ app/src/main/java/ehersenaw/com/github/shoose/TournamentFragment.java
@@ -0,0 +1,20 @@
+package ehersenaw.com.github.shoose;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TournamentFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.tournament_fragment,container,false);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
